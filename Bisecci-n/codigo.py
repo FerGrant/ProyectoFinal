f170f6ba43478d1ee395b4e1bf9c1df8036228a1
Fernando Ruiz <42146869+FerGrant@users.noreply.github.com>
2018-11-26 10:31:26 -0600

    ¿Qué es el método de la Bisección?
    En matemáticas, el método de bisección es un algoritmo de búsqueda de raíces que trabaja dividiendo el intervalo a la mitad y seleccionando el subintervalo que tiene la raíz.

--- Bisecci-n/codigo.py
+++ Bisecci-n/codigo.py
@@ -1,8 +1,3 @@
-##
-En matemáticas, el método de bisección es un algoritmo de búsqueda de raíces que trabaja
-dividiendo el intervalo a la mitad y seleccionando el subintervalo que tiene la raíz.
-##
-
 import math
 import numpy as np
 import matplotlib.pyplot as plt
